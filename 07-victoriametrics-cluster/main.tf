d002ccc704f0f01f911b81e343c9b99baa606756
Anton Patsev <patsev.anton@gmail.com>
2022-03-27 21:45:06 +0600

    resource "yandex_compute_instance" "vminsert"

--- 07-victoriametrics-cluster/main.tf
+++ 07-victoriametrics-cluster/main.tf
@@ -3,10 +3,84 @@ data "yandex_compute_image" family_images_linux {
 }
 
 resource "yandex_compute_instance" "vmstorage" {
-  count              = 1
+  count              = 4
   name               = "vmstorage${count.index}"
   platform_id        = "standard-v3"
   hostname           = "vmstorage${count.index}"
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  resources {
+    cores  = var.cores
+    memory = var.memory
+  }
+  boot_disk {
+    initialize_params {
+      size     = var.disk_size
+      type     = var.disk_type
+      image_id = data.yandex_compute_image.family_images_linux.id
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = self.network_interface.0.nat_ip_address
+      private_key = file("~/.ssh/id_rsa")
+    }
+    inline = [
+      "echo hello"
+    ]
+  }
+}
+
+resource "yandex_compute_instance" "vminsert" {
+  count              = 2
+  name               = "vminsert${count.index}"
+  platform_id        = "standard-v3"
+  hostname           = "vminsert${count.index}"
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  resources {
+    cores  = var.cores
+    memory = var.memory
+  }
+  boot_disk {
+    initialize_params {
+      size     = var.disk_size
+      type     = var.disk_type
+      image_id = data.yandex_compute_image.family_images_linux.id
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = self.network_interface.0.nat_ip_address
+      private_key = file("~/.ssh/id_rsa")
+    }
+    inline = [
+      "echo hello"
+    ]
+  }
+}
+
+resource "yandex_compute_instance" "vmselect" {
+  count              = 2
+  name               = "vmselect${count.index}"
+  platform_id        = "standard-v3"
+  hostname           = "vmselect${count.index}"
   service_account_id = yandex_iam_service_account.sa-compute-admin.id
   resources {
     cores  = var.cores
