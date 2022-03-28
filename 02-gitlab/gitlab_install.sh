1615784e2121e79229b4d9824cd20180db07ab4f
Anton Patsev <patsev.anton@gmail.com>
2022-03-28 12:13:31 +0600

    role: buluma.gitlab

--- 02-gitlab/gitlab_install.sh
+++ 02-gitlab/gitlab_install.sh
@@ -6,7 +6,7 @@ start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
-ansible-galaxy install robertdebock.gitlab
+ansible-galaxy install buluma.gitlab
 ansible-playbook -i inventory.yml playbook.yml
 end_time=`date +%s`
 date2=$(date +"%s")
