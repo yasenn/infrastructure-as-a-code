19f229aa01630b7ed2a774eb677b69dadd6facd5
Anton Patsev <patsev.anton@gmail.com>
2022-03-29 17:17:02 +0600

    ansible-galaxy install --force git+https://github.com/bmillemathias/ansible-role-seaweedfs.git,main

--- seaweedfs/seaweedfs_install.sh
+++ seaweedfs/seaweedfs_install.sh
@@ -6,7 +6,7 @@ start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
-ansible-galaxy install bmillemathias.seaweedfs
+ansible-galaxy install --force git+https://github.com/bmillemathias/ansible-role-seaweedfs.git,main
 ansible-playbook -i inventory.yml playbook.yml -bK
 end_time=`date +%s`
 date2=$(date +"%s")
