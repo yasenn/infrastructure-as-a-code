88933e2d3f884f5a42bc54d520b4fa301a7f01ab
Anton Patsev <patsev.anton@gmail.com>
2022-04-01 11:01:26 +0600

    git clone https://github.com/opensearch-project/ansible-playbook.git opensearch-project

--- opensearch-cluster/opensearch_cluster_destroy.sh
+++ opensearch-cluster/opensearch_cluster_destroy.sh
@@ -5,6 +5,7 @@ set -e
 start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform destroy -auto-approve
+rm -rf opensearch-project || true
 end_time=`date +%s`
 date2=$(date +"%s")
 echo "###############"
--- opensearch-cluster/opensearch_cluster_install.sh
+++ opensearch-cluster/opensearch_cluster_install.sh
@@ -6,9 +6,9 @@ start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
-ansible-galaxy install buluma.java
-ansible-galaxy install elastic.opensearch,v7.17.0
-ansible-playbook -i host.ini playbook.yml
+rm -rf opensearch-project || true
+git clone https://github.com/opensearch-project/ansible-playbook.git opensearch-project
+ansible-playbook -i host.ini opensearch-project/opensearch.yml
 end_time=`date +%s`
 date2=$(date +"%s")
 echo "###############"
