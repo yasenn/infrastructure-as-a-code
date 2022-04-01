dee2c7c64c6cc3ad5ab14bb6f72d2b430eb344a8
Anton Patsev <patsev.anton@gmail.com>
2022-04-01 10:35:07 +0600

    opensearch-cluster

--- opensearch-cluster/main.tf
+++ opensearch-cluster/main.tf
@@ -2,7 +2,7 @@ data "yandex_compute_image" family_images_linux {
   family = var.family_images_linux
 }
 
-resource "yandex_compute_instance" "opensearch" {
+resource "yandex_compute_instance" "master" {
   count              = 3
   name               = "opensearch${count.index}"
   platform_id        = "standard-v3"
@@ -39,6 +39,81 @@ resource "yandex_compute_instance" "opensearch" {
   }
 }
 
+resource "yandex_compute_instance" "data" {
+  count              = 2
+  name               = "data${count.index}"
+  platform_id        = "standard-v3"
+  hostname           = "data${count.index}"
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  resources {
+    cores  = var.cores
+    memory = var.memory
+  }
+  boot_disk {
+    initialize_params {
+      size     = var.disk_size
+      type     = var.disk_type
+      image_id = data.yandex_compute_image.family_images_linux.id
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = self.network_interface.0.nat_ip_address
+      private_key = file("~/.ssh/id_rsa")
+    }
+    inline = [
+      "echo check connection"
+    ]
+  }
+}
+
+resource "yandex_compute_instance" "dashboard" {
+  count              = 2
+  name               = "dashboard${count.index}"
+  platform_id        = "standard-v3"
+  hostname           = "dashboard${count.index}"
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  resources {
+    cores  = var.cores
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+      size     = var.disk_size
+      type     = var.disk_type
+      image_id = data.yandex_compute_image.family_images_linux.id
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = self.network_interface.0.nat_ip_address
+      private_key = file("~/.ssh/id_rsa")
+    }
+    inline = [
+      "echo check connection"
+    ]
+  }
+}
+
+
 resource "yandex_vpc_network" "network-1" {
   name = "network1"
 }
@@ -59,9 +134,35 @@ resource "yandex_vpc_subnet" "subnet-1" {
 resource "local_file" "host_ini" {
   filename = "host.ini"
   content = <<-EOT
-[opensearch]
-%{ for index, node in yandex_compute_instance.opensearch ~}
-${ node.name } ansible_host=${ node.network_interface.0.nat_ip_address }
+%{ for index, node in yandex_compute_instance.master ~}
+${ node.name } ansible_host=${ node.network_interface.0.nat_ip_address } ip=${ node.network_interface.0.ip_address } roles=master,ingest
+%{ endfor ~}
+%{ for index, node in yandex_compute_instance.data ~}
+${ node.name } ansible_host=${ node.network_interface.0.nat_ip_address } ip=${ node.network_interface.0.ip_address } roles=data
+%{ endfor ~}
+%{ for index, node in yandex_compute_instance.dashboard ~}
+${ node.name } ansible_host=${ node.network_interface.0.nat_ip_address } ip=${ node.network_interface.0.ip_address }
+%{ endfor ~}
+
+[os-cluster]
+%{ for index, node in yandex_compute_instance.master ~}
+${ node.name }
+%{ endfor ~}
+%{ for index, node in yandex_compute_instance.data ~}
+${ node.name }
+%{ endfor ~}
+%{ for index, node in yandex_compute_instance.dashboard ~}
+${ node.name }
+%{ endfor ~}
+
+[master]
+%{ for index, node in yandex_compute_instance.master ~}
+${ node.name }
+%{ endfor ~}
+
+[dashboard]
+%{ for index, node in yandex_compute_instance.dashboard ~}
+${ node.name }
 %{ endfor ~}
 
 [all:vars]
