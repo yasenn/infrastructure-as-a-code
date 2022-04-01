a100c646fc5b141760454d02033e1978f60e403b
Anton Patsev <patsev.anton@gmail.com>
2022-04-01 22:40:08 +0600

    git clone https://github.com/dpvpro/ansible-clickhouse-dp.git || true

--- clickhouse-cluster/clickhouse_destroy.sh
+++ clickhouse-cluster/clickhouse_destroy.sh
@@ -5,6 +5,7 @@ set -e
 start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform destroy -auto-approve
+rm -rf ansible-clickhouse-dp
 end_time=`date +%s`
 date2=$(date +"%s")
 echo "###############"
