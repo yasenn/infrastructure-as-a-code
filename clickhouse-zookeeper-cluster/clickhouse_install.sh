56213dbfd7d45af416c714d8f5d3b76c9681f938
Anton Patsev <patsev.anton@gmail.com>
2022-04-05 19:06:36 +0600

    zookeepers in playbook for clickhouse cluster

--- clickhouse-zookeeper-cluster/clickhouse_install.sh
+++ clickhouse-zookeeper-cluster/clickhouse_install.sh
@@ -6,6 +6,8 @@ start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
+ansible-galaxy install buluma.java
+ansible-galaxy install idealista.zookeeper_role
 ansible-galaxy install git+https://github.com/AlexeySetevoi/ansible-clickhouse.git,master
 ansible-playbook -i inventory.yml playbook.yml
 end_time=`date +%s`
