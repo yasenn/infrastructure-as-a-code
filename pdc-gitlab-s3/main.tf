c81f2d8828ff4d604b7771de9080ea5e5dd6eba2
Anton Patsev <patsev.anton@gmail.com>
2022-04-06 11:39:46 +0600

    Remove host_ini from pdc gitlab s3

--- pdc-gitlab-s3/main.tf
+++ pdc-gitlab-s3/main.tf
@@ -131,31 +131,6 @@ output "public_ip_gitlab" {
   value       = yandex_compute_instance.gitlab.network_interface.0.nat_ip_address
 }
 
-resource "local_file" "host_ini" {
-  content  = data.template_file.host_ini.rendered
-  filename = "host.ini"
-}
-
-data "template_file" "host_ini" {
-  template = file("host_ini.tmpl")
-  vars = {
-    windows_password      = var.windows_password
-    pdc_hostname          = var.pdc_hostname
-    pdc_domain            = var.pdc_domain
-    pdc_domain_path       = var.pdc_domain_path
-    public_ip_pdc         = yandex_compute_instance.active_directory.network_interface.0.nat_ip_address
-    gitlab_hostname       = var.gitlab_hostname
-    public_ip_gitlab      = yandex_compute_instance.gitlab.network_interface.0.nat_ip_address
-    letsencrypt_domain    = var.letsencrypt_domain
-    pswd_gitlab_ldap_sync = var.pswd_gitlab_ldap_sync
-    pswd_test_user_in_pdc = var.pswd_test_user_in_pdc
-    aws_access_key_id     = yandex_storage_bucket.gitlab-backup-anton-patsev.access_key
-    aws_secret_access_key = yandex_storage_bucket.gitlab-backup-anton-patsev.secret_key
-    gitlab_backup_bucket_name = "gitlab-backup-anton-patsev"
-    
-  }
-}
-
 resource "local_file" "inventory_yml" {
   content  = data.template_file.inventory_yml.rendered
   filename = "inventory.yml"
