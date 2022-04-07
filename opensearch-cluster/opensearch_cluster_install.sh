26663fb619663db4ec4d47b1f54d53fa81025ca9
Anton Patsev <patsev.anton@gmail.com>
2022-04-07 20:24:56 +0600

    git clone https://github.com/opensearch-project/ansible-playbook.git opensearch-project

--- opensearch-cluster/opensearch_cluster_install.sh
+++ opensearch-cluster/opensearch_cluster_install.sh
@@ -7,7 +7,7 @@ date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
 rm -rf opensearch-project || true
-git clone https://github.com/patsevanton/ansible-playbook.git opensearch-project
+git clone https://github.com/opensearch-project/ansible-playbook.git opensearch-project
 ansible-playbook -i host.ini opensearch-project/opensearch.yml -b
 end_time=`date +%s`
 date2=$(date +"%s")
