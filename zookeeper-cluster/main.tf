f919256d0007e053fdfc363616ce298224876836
Anton Patsev <patsev.anton@gmail.com>
2022-04-08 20:14:57 +0600

    templatefile zookeeper-cluster

--- zookeeper-cluster/main.tf
+++ zookeeper-cluster/main.tf
@@ -50,37 +50,13 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-# output "public_ip" {
-#   description = "Public IP address for active directory"
-#   value       = yandex_compute_instance.victoriametrics_cluster[*].network_interface.0.nat_ip_address
-# }
-
-# resource "local_file" "host_ini" {
-#   filename = "host.ini"
-#   content = <<-EOT
-# [zookeepers]
-# %{ for index, node in yandex_compute_instance.zookeeper ~}
-# ${ node.name } ansible_host=${ node.network_interface.0.nat_ip_address }
-# %{ endfor ~}
-
-# # [zookeeper-nodes]
-# # zoo1 zookeeper_id=1
-# # zoo2 zookeeper_id=2
-# # zoo3 zookeeper_id=3
-
-# [all:vars]
-# ansible_user=ubuntu
-# ansible_ssh_private_key_file=~/.ssh/id_rsa
-#   EOT
-# }
-
 
 resource "local_file" "inventory_yml" {
-  content = templatefile("inventory_yml.tmpl", { content = tomap({
-    for index, node in yandex_compute_instance.zookeeper :
-    index => node.network_interface.0.nat_ip_address
-    })
-  })
+  content = templatefile("inventory_yml.tmpl",
+    {
+      ssh_user            = var.ssh_user
+      zookeeper_public_ip = yandex_compute_instance.zookeeper.*.network_interface.0.nat_ip_address
+    }
+  )
   filename = "inventory.yml"
 }
-
