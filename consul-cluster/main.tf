6e9937e7f55cc6cb101fd1015faeb7d9f0df3f03
Anton Patsev <patsev.anton@gmail.com>
2022-04-08 15:11:20 +0600

    templatefile consul

--- consul-cluster/main.tf
+++ consul-cluster/main.tf
@@ -51,10 +51,11 @@ resource "yandex_vpc_subnet" "subnet-1" {
 }
 
 resource "local_file" "inventory_yml" {
-  content = templatefile("inventory_yml.tmpl", { content = tomap({
-    for index, node in yandex_compute_instance.consul:
-      index => node.network_interface.0.nat_ip_address
-    })
-  })
+  content = templatefile("inventory_yml.tmpl",
+    {
+      ssh_user             = var.ssh_user
+      consul_public_ip = yandex_compute_instance.consul.*.network_interface.0.nat_ip_address
+    }
+  )
   filename = "inventory.yml"
 }
