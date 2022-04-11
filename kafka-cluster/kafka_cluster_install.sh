82a4f41c7ac05d28f4ceea1e6695edffaa17d912
Anton Patsev <patsev.anton@gmail.com>
2022-04-11 08:06:40 +0600

    ansible-galaxy install sleighzy.kafka

--- kafka-cluster/kafka_cluster_install.sh
+++ kafka-cluster/kafka_cluster_install.sh
@@ -8,7 +8,7 @@ TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
 ansible-galaxy install buluma.java
 ansible-galaxy install idealista.zookeeper_role
-ansible-galaxy install sleighzy.zookeeper
+ansible-galaxy install sleighzy.kafka
 ansible-playbook -i inventory.yml playbook.yml
 end_time=`date +%s`
 date2=$(date +"%s")
