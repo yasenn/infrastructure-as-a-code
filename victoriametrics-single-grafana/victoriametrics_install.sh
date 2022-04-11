ce574ff4ad44099e4c2e6fd187565136b4a38406
Anton Patsev <patsev.anton@gmail.com>
2022-04-11 18:46:22 +0600

    node_exporter  victoriametrics-single-grafana

--- victoriametrics-single-grafana/victoriametrics_install.sh
+++ victoriametrics-single-grafana/victoriametrics_install.sh
@@ -6,6 +6,7 @@ start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
+ansible-galaxy install cloudalchemy.node_exporter
 ansible-galaxy collection install victoriametrics.cluster
 ansible-galaxy install buluma.grafana
 ansible-playbook -i inventory.yml playbook.yml
